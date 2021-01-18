4b4c2f34f5402393837c4248972cca6d035f6e17
lisanerd <lisa.rm.nyc@gmail.com>
2021-01-18 18:02:58 -0500

    Actually, I started with the stepper motor example from the library.

--- future_city_arduino_motor/future_city_arduino_motor.ino
+++ future_city_arduino_motor/future_city_arduino_motor.ino
@@ -1,116 +1,43 @@
 
+/*
+ Stepper Motor Control - one revolution
+
+ This program drives a unipolar or bipolar stepper motor.
+ The motor is attached to digital pins 8 - 11 of the Arduino.
+
+ The motor should revolve one revolution in one direction, then
+ one revolution in the other direction.
 
 
-#define IN1  8
-#define IN2  9
-#define IN3  10
-#define IN4  11
-int Steps = 0;
-boolean Direction = true;// gre
-unsigned long last_time;
-unsigned long currentMillis ;
-int steps_left = 4095;
-long time;
-void setup()
-{
-  Serial.begin(115200);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  // delay(1000);
+ Created 11 Mar. 2007
+ Modified 30 Nov. 2009
+ by Tom Igoe
 
-}
-void loop()
-{
-  while (steps_left > 0) {
-    currentMillis = micros();
-    if (currentMillis - last_time >= 1000) {
-      stepper(1);
-      time = time + micros() - last_time;
-      last_time = micros();
-      steps_left--;
-    }
-  }
-  Serial.println(time);
-  Serial.println("Wait...!");
-  delay(2000);
-  Direction = !Direction;
-  steps_left = 4095;
+ */
+
+#include <Stepper.h>
+
+const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+void setup() {
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(5);
+  // initialize the serial port:
+  Serial.begin(9600);
 }
 
-void stepper(int xw) {
-  for (int x = 0; x < xw; x++) {
-    switch (Steps) {
-      case 0:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, HIGH);
-        break;
-      case 1:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, HIGH);
-        digitalWrite(IN4, HIGH);
-        break;
-      case 2:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, HIGH);
-        digitalWrite(IN4, LOW);
-        break;
-      case 3:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, HIGH);
-        digitalWrite(IN3, HIGH);
-        digitalWrite(IN4, LOW);
-        break;
-      case 4:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, HIGH);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
-        break;
-      case 5:
-        digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, HIGH);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
-        break;
-      case 6:
-        digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
-        break;
-      case 7:
-        digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, HIGH);
-        break;
-      default:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
-        break;
-    }
-    SetDirection();
-  }
-}
-void SetDirection() {
-  if (Direction == 1) {
-    Steps++;
-  }
-  if (Direction == 0) {
-    Steps--;
-  }
-  if (Steps > 7) {
-    Steps = 0;
-  }
-  if (Steps < 0) {
-    Steps = 7;
-  }
+void loop() {
+  // step one revolution  in one direction:
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
 }
